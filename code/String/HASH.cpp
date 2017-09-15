cc529563ca8cf45a2e7d53c2d2fb930ea2164aa7
Tianyu Wang <zxr960115@gmail.com>
2017-09-15 19:18:12 +0800

    Update HASH.cpp

--- code/String/HASH.cpp
+++ code/String/HASH.cpp
@@ -1,33 +1,19 @@
-const int N = 400000, M = 5000000, HEAD = 399997;
-const int P = 1e9 + 7;
-struct HASH {
-	int cnt, head[N], next[M], len[M];
-	LL key[M];
-	HASH() {
-		clear();
-	}
-	inline void clear() {
-		memset (head, -1, sizeof head);
-		cnt = 0;
-	}
-	inline void ADD(int x, LL y, int w) {
-		key[cnt] = y;
-		next[cnt] = head[x];
-		len[cnt] = w;
-		head[x] = cnt ++;
-	}
-	inline int GETHEAD(LL idx) {
-		return idx % HEAD;
-	}
-	inline void add(LL idx, int w) {
-		int h = GETHEAD(idx);
-		ADD(h, idx, w);
-	}
-	bool find(LL idx, int w) {
-		int h = GETHEAD(idx);
-		for (int i = head[h]; ~ i; i = next[i])
-			if (key[i] == idx && len[i] == w)
-				return true;
-		return false;
-	}
-}mp;
+pw[0] = 1;
+for (int i = 1; i < 2001; i ++)
+{
+	pw[i] = pw[i - 1] * MAGIC;
+}
+h[ls] = 0;
+for (int i = ls - 1; i >= 0; i --)
+{
+	h[i] = h[i + 1] * MAGIC + s[i];
+}
+
+const int N = 2100;
+LL pw[N], h[N];
+const LL MAGIC = 131;
+
+LL hash(int i, int l)
+{
+	return h[i] - h[i + l] * pw[l];
+}
